149eab843df182f2e9b98127ce4a062f5d81aa96
Brandon C <brancarp93@gmail.com>
2022-02-15 11:23:15 -0500

    updated date function

--- src/Components/Expenses/ExpenseDate.js
+++ src/Components/Expenses/ExpenseDate.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import {React} from 'react';
 const ExpenseDate = (props) => {
-  const month = new Date(props.date).toLocaleDateString("en-us", {
+
+  const date = new Date(props.date);
+  date.setMinutes(date.getMinutes() + date.getTimezoneOffset() );
+
+  const month = new Date(date).toLocaleDateString("en-us", {
     month: "long",
   });
-  const day = new Date(props.date).toLocaleString("en-US", { day: "2-digit" });
-  const year = new Date(props.date).getFullYear();
-
-  const newDay = parseInt(day) + 1;
+  const day = new Date(date).toLocaleString("en-US", { day: "2-digit" });
+  const year = new Date(date).getFullYear();
+  
 
   return (
       <div className='d-flex justify-content-center align-items-center flex-column text-primary'>
       <div>{month}</div>
-      <div>{newDay}</div>
+      <div>{day}</div>
       <div>{year}</div>
     </div>
   );
